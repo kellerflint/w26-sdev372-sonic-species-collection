a9d6a172b9f196ef3932a68393703b85526f6e49
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2026-01-13 14:41:23 -0800

    database connection file created

---
+++ src/db/connection.js
@@ -0,0 +1,23 @@
+import { Sequelize } from "sequelize";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME, DB_DIALECT } = process.env;
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+    host: DB_HOST,
+    port: DB_PORT,
+    dialect: DB_DIALECT,
+    logging: q => console.log(`Squelize query: ${q}`)
+});
+
+try {
+    await sequelize.authenticate();
+    console.log("Connected to MYSQL DB!")
+} catch (err) {
+    console.log("Can't connect to DB!");
+    console.log(err);
+}
+
+export default sequelize
