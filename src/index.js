2cb9615fefe6b8e8de7ff1304d8bb8b7183ee5d9
BradyLeg <184549450+BradyLeg@users.noreply.github.com>
2026-01-19 19:41:01 -0800

    Added /api/name route that retrives a species name as a query and returns all characters of that species

--- src/index.js
+++ src/index.js
@@ -21,6 +21,16 @@ app.get("/api/all", async (req, res) => {
     console.log("Species returned");
 })
 
+app.get("/api/name", async (req, res) => {
+    const name = req.query.name
+
+    const data = await schema.findAll({
+        where: { name }
+    });
+
+    res.json(data);
+})
+
 const PORT = process.env.API_PORT || 3001;
 
 app.listen(PORT, async () => {
