1ec861a8cbc13161ecf3ee667bd37021daa0e781
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2026-01-21 01:25:44 -0800

    Seperated front and back-end, as well as the routes from the control

---
+++ src/back-end/index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import sequelize from "./db/connection.js";
+import router from "./routes/router.js";
+
+
+const app = express();
+
+app.use(cors({ origin: "http://localhost:5173" }));
+app.use(express.json());
+app.use('/', router);
+
+
+
+const PORT = process.env.API_PORT || 3001;
+
+app.listen(PORT, async () => {
+    console.log(`API running on port ${PORT}`)
+    await sequelize.authenticate();
+    await sequelize.sync();
+    console.log("Connected to MYSQL DB!")
+})
