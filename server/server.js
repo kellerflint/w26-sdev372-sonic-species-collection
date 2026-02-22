7124f1a2f70de82a5e3358aeab03b9e993a39b18
Gabby-Moon <194023457+Gabby-Moon@users.noreply.github.com>
2026-02-21 19:56:00 -0800

    Fixed some things with docker files and leftovers

--- server/server.js
+++ server/server.js
@@ -17,10 +17,7 @@ app.use('/', router);
 
 const PORT = process.env.API_PORT || 3001;
 
-app.use(express.static("public"));
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"))
-})
+
 
 app.listen(PORT, async () => {
     console.log(`API running on port ${PORT}`)
